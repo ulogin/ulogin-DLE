f44140da96d8499f545c7fd97e56600811004896
uLogin Team <team@ulogin.ru>
2012-05-12 14:47:13 +0400

    update commit

--- ulogin.php
+++ ulogin.php
@@ -3,7 +3,7 @@
 //=====================================================
 // ULogin DataLife Engine 9.4
 //-----------------------------------------------------
-// ÐœÐ¾Ð´ÑƒÐ»ÑŒ Ð°Ð²Ñ‚Ð¾Ñ€Ð¸Ð·Ð°Ñ†Ð¸Ð¸ Ð¸ Ñ€ÐµÐ³Ð¸ÑÑ‚Ñ€Ð°Ñ†Ð¸Ð¸ Ð¿Ñ€Ð¸ Ð¿Ð¾Ð¼Ð¾Ñ‰Ð¸ uLogin
+// Ìîäóëü àâòîðèçàöèè è ðåãèñòðàöèè ïðè ïîìîùè uLogin
 //-----------------------------------------------------
 // http://ulogin.ru/
 // team@ulogin.ru
@@ -18,8 +18,14 @@ if(!defined('DATALIFEENGINE'))
 	die("Hacking attempt!");
 }
 
+require_once ENGINE_DIR . '/classes/parse.class.php';
 
-function register_user($id,$pass) {
+$parse = new ParseFilter( );
+$parse->safe_mode = true;
+$parse->allow_url = false;
+$parse->allow_image = false;
+
+function login_user($id,$pass) {
 	global $db;
 	global $config;
 	global $is_logged;
@@ -68,6 +74,26 @@ function register_user($id,$pass) {
 	$is_logged = TRUE;
 }
 
+function check_reg($name, $email) {
+	global $lang, $db, $banned_info, $relates_word;
+	$stop = false;
+	
+	if( strlen( $name ) > 20 ) @$name = substr($name, 0, 20);
+	if( preg_match( "/[\||\'|\<|\>|\[|\]|\"|\!|\?|\$|\@|\/|\\\|\&\~\*\{\+]/", $name ) ) @$name = preg_replace("/[\||\'|\<|\>|\[|\]|\"|\!|\?|\$|\@|\/|\\\|\&\~\*\{\+]/",'',$name);
+	if (strpos( strtolower ($name) , '.php' ) !== false) {
+            @$name = str_replace('.php', '', $name);
+            @$name = str_replace('.PHP', '', $name);
+        }
+	
+	$name = strtolower( $name );
+        $search_name = strtr( $name, $relates_word );
+		
+	$row = $db->super_query( "SELECT COUNT(*) as count FROM " . USERPREFIX . "_users WHERE email = '$email' OR LOWER(name) REGEXP '[[:<:]]{$search_name}[[:>:]]' OR name = '$name'" );
+	if( $row['count'] ) $stop = true;
+
+	return $stop;
+
+}
 
 if(isset($_POST['token'])){
 
@@ -75,62 +101,53 @@ if(isset($_POST['token'])){
 
 	$s = file_get_contents('http://ulogin.ru/token.php?token=' . $_POST['token'] . '&host=' . $_SERVER['HTTP_HOST']);
 	$user = json_decode($s, true);
-
-	$member_id = $db->super_query( "SELECT user_id,seed FROM " . USERPREFIX . "_ulogin where ident='".$db->safesql($user['identity'])."'" );
-	if($member_id) {
-		$password=md5($user['identity'].$member_id['seed']);
-		$member_id = $db->super_query( "SELECT user_id FROM " . USERPREFIX . "_users where user_id=".$member_id['user_id'] );
-		if(!$member_id) {
-			$db->query('delete from '.USERPREFIX.'_ulogin where ident=\''.$db->safesql($user['identity'])."'");
-		}
+        if (isset($user['error'])){
+            
+        }
+        $db->super_query("CREATE TABLE IF NOT EXISTS `".USERPREFIX."_ulogin` (
+                              `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                              `user_id` int(10) unsigned NOT NULL,
+                              `ident` char(255) NOT NULL,
+                              `email` char(255) DEFAULT NULL,
+                              `seed` int(10) unsigned NOT NULL,
+                              PRIMARY KEY (`id`)
+                            ) ENGINE=MyISAM;");
+	$ulogin_id = $db->super_query( "SELECT user_id,seed FROM " . USERPREFIX . "_ulogin where ident='".$db->safesql($user['identity'])."'" );
+        $member_id = FALSE;
+        if($ulogin_id) {
+		$password = md5($user['identity'].$ulogin_id['seed']);
+		$member_id = $db->super_query( "SELECT user_id FROM " . USERPREFIX . "_users where user_id=".$ulogin_id['user_id'] );
 	}
 
-
 	if($member_id)
-		register_user($member_id['user_id'],$password);
+		login_user($member_id['user_id'],$password);
 	else {
-	
-	//For Non-Unicode version of DLE:
-		$fullname = convert_unicode($user['first_name'].' '.$user['last_name'] );
-		$login=convert_unicode($user['first_name'].'_'.$user['last_name']);
-	//For Unicode version of DLE:
-	//	$fullname = $user['first_name'].' '.$user['last_name'];
-	//	$login=$user['first_name'].'_'.$user['last_name'];
-		
-		
-		$fullname=addslashes($fullname);
-		$login=addslashes($login);
-		
+		$fullname = $config['charset'] != 'utf-8' ? convert_unicode($user['first_name'].' '.$user['last_name'], $config['charset']) : $user['first_name'].' '.$user['last_name'];
+                $fullname = $db->safesql( $parse->process( $fullname) );
+                $login = isset ($user['nickname']) ? $user['nickname'] : $user['first_name'];
+		$login = $config['charset'] != 'utf-8' ? convert_unicode($login) : $login;
+                $login = $db->safesql( $parse->process( htmlspecialchars( trim( $login ) ) ) );
+                $login = preg_replace('#\s+#i', ' ', $login);
+
+                $not_allow_symbol = array ("\x22", "\x60", "\t", '\n', '\r', "\n", "\r", '\\', ",", "/", "¬", "#", ";", ":", "~", "[", "]", "{", "}", ")", "(", "*", "^", "%", "$", "<", ">", "?", "!", '"', "'", " " );
+                $email = $user['email'];
+                $email = $db->safesql(trim( str_replace( $not_allow_symbol, '', strip_tags( stripslashes( $email)))));
+                
+                
 		if(isset($user['photo'])){
 			$photo = $user['photo'];
 		} else $photo ="";
-		$email = $user['email'];
-		
-		$cnt=$db->super_query( "SELECT COUNT(*) as how FROM " . USERPREFIX . "_users where email='".$email."'" );
-		if($cnt['how']>0){
-			$res=preg_match('/^([^\@]+)\@([^\@]+)$/',$email,$matches);
-			$nemail=$matches[1].'_'.$user['network'];
-			$i=0;
-			do {
-				$email=$nemail;
-				$cnt=$db->super_query( "SELECT COUNT(*) as how FROM " . USERPREFIX . "_users where email='".$db->safesql($email.'@'.$matches[2])."'" );
-				$nemail=$matches[1].'_'.$user['network'].'@'.$matches[2];
-				$i++;
-				$nemail=$matches[1].'_'.$user['network'].'_'.$i;
-			} while($cnt['how']>0);
-			$email.='@'.$matches[2];
-		}
-		
-		$cnt=$db->super_query( "SELECT COUNT(*) as how FROM " . USERPREFIX . "_users where name='".$login."'" );
-		if($cnt['how']>0){
-			$i=0;
-			do {
-				$i++;
-				$cnt=$db->super_query( "SELECT COUNT(*) as how FROM " . USERPREFIX . "_users where name='".$db->safesql($login.'_'.$i)."'" );				
-			} while($cnt['how']>0);
-			$login.='_'.$i;
-		}
-			
+                
+                $idx = 0;
+                $email_parts = explode('@', $email);
+                $test_login = $login;
+		while ($reg_error = check_reg($test_login, $email)){
+                    $idx ++;
+                    $test_login = $login.'_'.$idx;
+                    $email = $email_parts[0].'+'.$test_login.'@'.$email_parts[1];
+                }
+                $login = $test_login;    
+                
 		$stopregistration = false;
 
 		$config['reg_group_ulogin'] = intval( $config['reg_group_ulogin'] ) ? intval( $config['reg_group_ulogin'] ) : 4;
@@ -144,8 +161,12 @@ if(isset($_POST['token'])){
 		if( intval( $config['reg_group'] ) < 3 ) $config['reg_group'] = 4;
 			
 		$db->query( "INSERT INTO " . USERPREFIX . "_users (name, fullname, password, email, reg_date, lastdate, user_group, info, signature, favorites, xfields, logged_ip) VALUES ('$login', '$fullname', '$regpassword', '$email', '$add_time', '$add_time', '" . $config['reg_group_ulogin'] . "', '', '', '', '', '" . $_IP . "')" );
-		$user_id=$id = $db->insert_id();
-		$db->query("INSERT INTO " . USERPREFIX . "_ulogin (user_id, ident, email, seed) values ($id, '".$user['identity']."','".$user['email']."', $seed)");
+		$user_id = $id = $db->insert_id();
+                if ($ulogin_id)
+                    $db->query("UPDATE " . USERPREFIX . "_ulogin SET user_id =".$user_id." where ident ='".$db->safesql($user['identity'])."'");
+                else
+                    $db->query("INSERT INTO " . USERPREFIX . "_ulogin (user_id, ident, email, seed) values ($id, '".$user['identity']."','".$user['email']."', $seed)");
+                
 		$id++;
 		if( $photo ) {
 			$fparts = pathinfo($photo);
@@ -163,7 +184,7 @@ if(isset($_POST['token'])){
 				$db->query( "UPDATE " . USERPREFIX . "_users set foto='$foto_name' where user_id=$user_id" );
 			}
 		}
-		register_user($user_id,$password);
+		login_user($user_id,$password);
 	}
 	unset($_POST['token']);
 }
